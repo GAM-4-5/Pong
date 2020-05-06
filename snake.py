31a125d95834a8f1e242b4060ccf5cdc189fdf15
perki891 <62668216+perki891@users.noreply.github.com>
2020-05-06 13:05:28 +0200

    Add files via upload

--- snake.py
+++ snake.py
@@ -25,5 +25,63 @@ tDel.speed(0); tDel.shapesize(2.2,2.2); tDel.shape('square'); tDel.color('white'
 tHrana.speed(0); tHrana.shapesize(1.2,1.2); tHrana.shape('turtle'); tHrana.pu(); tHrana.ht()
 tMapa.speed(0); tMapa.width(35); tMapa.ht() #određivanje dimenzija za mapu
 
+#brzina zmije
+difficulty=4
+
+#crtanje mape
+def mapa():
+    tMapa.pu(); tMapa.goto(-350,350); tMapa.pd(); tMapa.color('lightgray')
+    tMapa.begin_fill()
+    for q in range(4):
+        tMapa.fd(700); tMapa.rt(90)
+    tMapa.end_fill()
+    tMapa.pu(); tMapa.goto(-250,250); tMapa.pd(); tMapa.color('black')
+    tMapa.begin_fill()
+    for q in range(4):
+        tMapa.color('black'); tMapa.fd(500); tMapa.rt(90)
+    tMapa.color('white'); tMapa.end_fill()
+    
+
+def roundto(x):
+    y=round(x/50)
+    return y*50
+
+#funkcije za kretanje zmije
+def up(a):
+    if turt.heading()==270:
+        return
+    else:
+        turt.seth(90)
+
+def right(a):
+    if turt.heading()==180:
+        return
+    else:
+        turt.seth(0)
+
+def down(a):
+    if turt.heading()==90:
+        return
+    else:
+        turt.seth(270)
+
+def left(a):
+    if turt.heading()==0:
+        return
+    else:
+        turt.seth(180)
+
+#restarta igricu i obnavlja početne vrijednosti
+def restart(a):
+    global putanja
+    restartButton.config(text='RESTARTING...', state='disabled', relief='sunken')
+    duljina.config(text='2/81')
+    win.config(text=' ')
+    turt.goto(-200,200); turt.seth(0)
+    putanja=[[-200,200]]
+    mapa(); hrana()
+    sleep(2)
+    restartButton.config(text='RESTART', state='active', relief='raised')
+    move()
 
 
